3d6e7bb9c469f33eaa7bbf184e7a4fa1f0629c37
Mbala <mbala@foxloft.com>
2020-05-28 22:37:45 -0500

    Weird undulating movement

--- nodes/demo.js
+++ nodes/demo.js
@@ -21,7 +21,12 @@ function draw(ctx) {
 
     const time = Date.now();
     for (let i = 0; i < 100; i++) {
-        node(ctx, 10 + (i % 10) * 90, Math.trunc(i / 10) * 85 +  100 + Math.sin(time / 1000.0) * 100, 80, 70, `hsl(${i * 4}, 100%, 50%)`);
+        node(ctx, 
+            10 + (i % 10) * 90 + Math.sin(i + time / 500.0) * 20, 
+            Math.trunc(i / 10) * 85 + 100 + Math.sin(time / 1000.0) * 100, 
+            80 + Math.sin(0.7 * i + time/ 200) * 5,
+            70 + Math.sin(0.3 * i + time/ 200) * 5,
+            `hsl(${i * 4}, 100%, 50%)`);
     }
     node(ctx, 400, 50, 100, 100);
     ctx.requestRedraw();
