bd565e3f8619f4a5fce44b907f917cf92e710c02
Mbala <mbala@foxloft.com>
2020-05-31 14:07:28 -0500

    Minor design tweaks

--- effect.js
+++ effect.js
@@ -3,5 +3,5 @@ config.width = 450;
 config.height = 450;
 config.numRectangles = 2000;
 config.backgroundColor = '#354147';
-config.color = (i, wv) => `rgb(34, 45, ${i % 10 !== 0 ? 61 : 10})`;
+config.color = (i, wv) => { const x = 23 + (i % 20); return `rgb(${x}, ${x + 6}, ${x + 12})`; };
 run();
